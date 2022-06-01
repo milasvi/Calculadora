f51fba946b85840529d7d66bc18c7e80eec7e007
Milena Sviatek <porstudag@gmail.com>
2022-06-01 11:58:48 -0300

    segundo commit

--- Calculadora.py
+++ Calculadora.py
@@ -1,7 +1,5 @@
-
+#bibliotecas
 import emoji
-
-
 
 #cabecalho
 print(emoji.emojize(':input_numbers::plus:CALCULADORA:plus::input_numbers:'))
@@ -17,15 +15,20 @@ while True:
     [4]Sair
     """)
     print('=-'*15)
+    #erro
     while escolha != '1' and escolha != '2'and escolha != '3' and escolha != '4':
         escolha = input('\033[0;31m Opcão inválida!\033[m')
+    #sair
     if escolha == '4':
         print('Obrigade por usar a CALCULADORA.\nSaindo...')
         break
+    #soma
     elif escolha == '1':
         print(f'Soma : {n1} + {n2} = {n1+n2}')
+    #multiplicação
     elif escolha == '2':
         print(f'Multiplicação: {n1} x {n2} = {n1*n2}')
+    #novos números
     else:
         n1 =int(input('Escreva o primeiro valor:'))
         n2 = int(input('Escreva o segundo valor:'))
